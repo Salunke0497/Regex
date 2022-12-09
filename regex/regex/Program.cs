9bc72df88beffbebf0ccb1e6d2574b21435c00a1
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-09 18:00:54 +0530

    welcome

--- regex/regex/Program.cs
+++ regex/regex/Program.cs
@@ -16,20 +16,30 @@ namespace regex
             Console.WriteLine("-------------");
             Validation Code = new Validation();
             //UC1 first name
+            //do
+            //{
+            //    Console.WriteLine("ENTER THE FIRST NAME");
+            //    string input = Convert.ToString(Console.ReadLine());
+            //    flag = Code.FirstName(input);
+            //} while (flag == true);
+
+            ////UC2 LAST NAME
+            //do
+            //{
+            //    Console.WriteLine("ENTER THE LAST NAME");
+            //    string input = Convert.ToString(Console.ReadLine());
+            //    flag = Code.LastName(input);
+            //} while (flag == true);
+
+            //UC3 mail-id
             do
             {
-                Console.WriteLine("ENTER THE FIRST NAME");
+                Console.WriteLine("ENTER THE E-Mail");
+                Console.WriteLine("formate--abc.xyz@bl.co.in");
                 string input = Convert.ToString(Console.ReadLine());
-                flag = Code.FirstName(input);
+                flag = Code.mail(input);
             } while (flag == true);
 
-            //UC2 LAST NAME
-            do
-            {
-                Console.WriteLine("ENTER THE LAST NAME");
-                string input = Convert.ToString(Console.ReadLine());
-                flag = Code.LastName(input);
-            } while (flag == true);
             Console.ReadLine();
         }
     }
--- regex/regex/Validation.cs
+++ regex/regex/Validation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace regex
 {
     internal class Validation
     {
-       const string expression = "^[A-Z][a-zA-Z]{3,}$";
+        const string expression = "^[A-Z][a-zA-Z]{3,}$";
+        const string expression2 = "^[a-zA-Z0-9]+[.]*[a-zA-Z0-9]*[@][a-zA-Z]+[.]?[a-zA-Z]+[.][a-zA-Z]*$";
+
         public bool FirstName(string input)
         {
             if (Regex.IsMatch(input, expression))
@@ -33,6 +36,19 @@ namespace regex
             else
             {
                 Console.WriteLine("LastName regex failed");
+                return true;
+            }
+        }
+        public bool mail(string input)
+        {
+            if (Regex.IsMatch(input, expression2))
+            {
+                Console.WriteLine("mail is correct");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("mail regex failed");
                 return true;
             }
         }
