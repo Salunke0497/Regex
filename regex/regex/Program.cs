f4c6f4c1f44e213390275e29798009bab2436f6b
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-09 13:02:23 +0530

    welcome

---
+++ regex/regex/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace regex
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("regex Experssion");
+            Console.WriteLine("-------------");
+            Validation Code = new Validation();
+
+            string input = "A40220";
+            Code.Test(input);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ regex/regex/Validation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace regex
+{
+    internal class Validation
+    {
+       const string PinCode = "^[0-9]{6}$";
+        public void Test(string input)
+        {
+         if (Regex.IsMatch(input, PinCode))
+            Console.WriteLine("PinCode is correct");
+         else
+            Console.WriteLine("PinCode regex failed");
+        }
+    }
+}
